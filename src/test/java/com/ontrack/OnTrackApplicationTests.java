92155a3cb577742f5fcfcb37610ee3000077b56b
MatthewWilsonBCAA <72146567+MatthewWilsonBCAA@users.noreply.github.com>
2021-06-09 09:07:45 -0500

    REEEEE

---
+++ src/test/java/com/ontrack/OnTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
