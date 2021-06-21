bd0b67358824d8d66d55264b6dbb0a07cc616a7b
MatthewWilsonBCAA <72146567+MatthewWilsonBCAA@users.noreply.github.com>
2021-06-21 11:20:27 -0500

    e

--- src/main/java/com/ontrack/PostRepository.java
+++ src/main/java/com/ontrack/PostRepository.java
@@ -2,7 +2,7 @@ package com.ontrack;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-public interface ThreadRepository extends JpaRepository<Thread, Long> {
+public interface PostRepository extends JpaRepository<Thread, Long> {
     //@Query("SELECT u FROM User u WHERE u.email = ?1")
 
 }
